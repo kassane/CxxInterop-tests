10d8f1ae8a45676ce178c12e7c0f36aac0065964
Yosh <github@yosh.is>
2023-05-12 02:21:33 +0200

    fancy cxx example

--- cxx/cpp/cats.cpp
+++ cxx/cpp/cats.cpp
@@ -1,8 +1,10 @@
-#include "../rust/target/cxxbridge/cats_cxx/src/main.rs.h"
+#include "../../target/cxxbridge/cats_cxx/src/main.rs.h"
 #include <iostream>
 void test()
 {
-    Cat marshmallow = make("Marshmallow");
-    marshmallow.meow();
-    std::cout << std::string(marshmallow.name());
+    Cat nori = make("Nori");
+    std::printf("Our cat's name is %s", std::string(nori.name()).c_str());
+    nori.meow();
+    nori.feed();
+    nori.meow();
 }
--- cxx/rust/src/main.rs
+++ cxx/rust/src/main.rs
@@ -2,12 +2,14 @@
 mod ffi {
     struct Cat {
         name_: String,
+        is_hungry: bool,
     }
 
     extern "Rust" {
         #[cxx_name = "make"]
         fn new_cat(name: &str) -> Cat;
         fn meow(self: &Cat);
+        fn feed(self: &mut Cat);
         fn name(self: &Cat) -> &str;
     }
 
@@ -20,16 +22,21 @@ mod ffi {
 fn new_cat(name: &str) -> ffi::Cat {
     ffi::Cat {
         name_: name.to_owned(),
+        is_hungry: true,
     }
 }
 
 impl ffi::Cat {
-    fn new(name: &str) -> ffi::Cat {
-        new_cat(name)
+    fn meow(&self) {
+        if self.is_hungry {
+            println!("{} is hungry", self.name_);
+        } else {
+            println!("{} is sleepy", self.name_);
+        }
     }
 
-    fn meow(&self) {
-        println!("meow\n");
+    fn feed(&mut self) {
+        self.is_hungry = false;
     }
 
     fn name(&self) -> &str {
